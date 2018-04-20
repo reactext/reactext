58872a6c9cf9c2013f2f607e5392e32bd7884d62
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-04-20 11:15:32 -0700

    moved files to chrome-ext folder

--- devtools.js
+++
@@ -1,5 +0,0 @@
-console.log("hello from devtools");
-chrome.devtools.panels.create("Reactext",
-                              "icon.png",
-                              "panel.html",
-                              function(panel) { console.log("hello from callback"); });
