056ff20ef78e410f854af89d2cc30319eafdfe42
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-04-20 11:28:30 -0700

    took request and require out of panel page

--- chrome-ext/panel.js
+++ chrome-ext/panel.js
@@ -1,18 +1,13 @@
 // const cheerio = require('cheerio');
-const request = require('request');
+// const request = require('request');
 
 console.log('panel.js here !!!');
 
 
 
-
-
-console.log(chrome.devtools.inspectedWindow.getResources((resources) => {
-    let res = resources.filter((el) => {
-        return el.url.includes('/bundle.js');
-    })
-    request(res.url, (error, response, html) => {
-        console.log(html);
-
-      });
-}));
+// console.log(chrome.devtools.inspectedWindow.getResources((resources) => {
+//     let res = resources.filter((el) => {
+//         return el.url.includes('/bundle.js');
+//     })
+//     console.log(res)
+// }));
