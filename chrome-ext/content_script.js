6c9f1519ccc4ec498d05c5620dcf2d13558e4780
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-04-23 20:26:00 -0700

    added state variable into function

--- chrome-ext/content_script.js
+++ chrome-ext/content_script.js
@@ -11,11 +11,9 @@ function injectScript(file, body) {
 
 injectScript(chrome.runtime.getURL('/hook.js'), 'body');
 
-var state;
-
 //ADD an eventListener that listens for Reactext event from hook.js and has a callback that sends a message to ?
 window.addEventListener("ReacText", (message) => {
   console.log('in contentScript, message:', message);
-  state = message.detail;
+  let state = message.detail;
   chrome.runtime.sendMessage({ data: state, from: 'content_script'});
 }, false);
