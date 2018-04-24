1a3cb88685a742d46231cb6e5cf96c79d5cf881b
Phillip Troutman <philliptroutman21@gmail.com>
2018-04-23 20:03:33 -0700

    added traverseAndGatherReactDom function

--- chrome-ext/hook.js
+++ chrome-ext/hook.js
@@ -26,6 +26,58 @@ let checkReactDOM =  (reactDOM) => {
     return data;
 }
 
+///////////////////////////////////////
+let traverseAndGatherReactDOM =  (node, cache) => {
+
+    let component = {
+        id:null,
+        name:'',
+        state:null,
+        props:null,
+        children:[],
+        store:null,
+    };
+
+    if (node._debugID) {
+        component.id = node._debugID
+    }
+
+    if (node.type) {
+        if (node.type.name) {
+            component.name = node.type.name;
+        } else {
+            component.name = node.type || 'Default';
+        }
+    }
+
+    if (node.memoizedState) {
+        component.state = node.memoizedState;
+    }
+
+    // redux store
+    if (node.type) {
+        if (node.type.name === 'Provider') {
+            reduxStore15 = node.stateNode.store.getState();
+        }
+    }
+
+    if (node.memoizedProps) {
+        // component.props = stringifyData(node.memoizedProps)
+        component.props = node.memoizedProps;
+    }
+
+    //if the current node has a child it runs it the function again on them
+    if (node.child !== null) {
+        traverseAndGatherReactDOM(node.child, component.children);
+    }
+
+    //if the current node has siblings it runs the function agin on them
+    if (node.sibling !== null) {
+        traverseAndGatherReactDOM(node.sibling, cache);
+    }
+
+    cache.push(component);
+}
 
 
 
