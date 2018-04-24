154adfdf397187e859a89004c8a6f2c2bc9a941a
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-04-23 20:22:14 -0700

    create a function to append script to windowBody

--- chrome-ext/content_script.js
+++ chrome-ext/content_script.js
@@ -1,1 +1,10 @@
 console.log('content.js here !!!');
+console.log("innnnn content_script.js");
+
+function injectScript(file, body) {
+  const windowBody = document.getElementsByTagName(body)[0];
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', file);
+  windowBody.appendChild(script);
+}
