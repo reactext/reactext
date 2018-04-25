e86014ad1911a40381856880eb29fdb1bbc03fe9
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-04-25 11:34:19 -0700

    rearrange function order to better understand flow

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -4,38 +4,40 @@ let ports = [];
 let state = [];
 
 //This listens for a chrom.runtime.onConnect to be fired
-chrome.runtime.onConnect.addListener(function(port) {
+chrome.runtime.onConnect.addListener(function (port) {
     console.log(port, '<-- im the port');
     console.log(ports, '<-- im the ports');
     if (port.name !== "devtools") return;
     ports.push(port);
     // Remove port when devtools is closed
-    port.onDisconnect.addListener(function() {
+    port.onDisconnect.addListener(function () {
         var i = ports.indexOf(port);
         if (i !== -1) ports.splice(i, 1);
     });
-    port.onMessage.addListener(function(msg) {
+    port.onMessage.addListener(function (msg) {
         // Received message from devtools. Do something:
         console.log('Received message from devtools page', msg);
     });
-    console.log('NEW STATEEEEE!!!!', state[state.length-1]);
-    notifyDevtools(JSON.stringify(state[state.length-1]));
+    console.log('NEW STATEEEEE!!!!', state[state.length - 1]);
+    notifyDevtools(JSON.stringify(state[state.length - 1]));
 });
 
 // Function to send a message to all devtools.html views:
 function notifyDevtools(msg) {
-    ports.forEach(function(port) {
+    ports.forEach(function (port) {
         port.postMessage(msg);
     });
 }
 
 //the following API receives a message from the content script 
 //a message is sent from hook.js -> content_script.js -> background.js EVERY TIME the page's state changes
-chrome.runtime.onMessage.addListener(function (msg, sender) {
+chrome.runtime.onMessage.addListener(function (msg, sender, res) {
+    console.log(res, '<reponse ------->')
     // validate we are listening for the correct msg
     if (msg.from === 'content_script') {
-      message = msg.data;
-      //message object from content_script is pushed to state array
-      state.push(message);
+        message = msg.data;
+        //message object from content_script is pushed to state array
+        state.push(message);
+        console.log(state, '<----this state array is growing')
     }
-  });
+});
