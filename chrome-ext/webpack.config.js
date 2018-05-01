9d16b6aa62ccdf1bae5a4d0ef6c069ffb5fedf95
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-05-01 11:04:25 -0700

    add entry, output and modele to wepback.config.js file

--- chrome-ext/webpack.config.js
+++ chrome-ext/webpack.config.js
@@ -1,3 +1,44 @@
 'use strict';
 
-let webpack = require('webpack')
+let webpack = require('webpack');
+
+module.exports = {
+    entry: {    
+        mode: 'development',
+        entry: './main.js',},
+    output: {
+        path: path.resolve(__dirname + '/build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader'
+            }
+            ]
+        },
+        {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            },
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        }
+        ]
+    },
+    devServer: {
+        publicPath: "/build/",
+        hot: true,
+        port: 3000
+    }
+};
