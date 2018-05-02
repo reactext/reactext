3a43062ea895cf45ff497d160a0cf34be7fb67f4
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-05-01 19:32:40 -0700

    added listener to index.js to grab initial state

--- chrome-ext/devtools.js
+++ chrome-ext/devtools.js
@@ -5,48 +5,59 @@ function createPanel() {
         '/panel.html',
         function (extensionPanel) {
             let _window; // This will hold the reference to panel.html's `window`
+            console.log('immmm the __window variable', _window);
+
             let data = [];
+
             //store tabId as variable
             let portId = JSON.stringify(chrome.devtools.inspectedWindow.tabId);
 
-            //connect to background.js with connect method, pass in optional object with name property
+            //connect to background.js with connect method, pass in object with name property
             let port = chrome.runtime.connect({ name: portId });
 
             console.log('im the port object', port);
 
             //show extension panel
             extensionPanel.onShown.addListener(function tmp(panelWindow) {
+                console.log('immmm in the extension panel on shown', panelWindow);
+
                 extensionPanel.onShown.removeListener(tmp); // Run once only
                 _window = panelWindow;
+                console.log('immmm the __window variable  line 2666666', _window);
 
                 // Release queued data
                 let msg;
                 while (msg = data.shift()) {
-                    console.log('mssssg in devtoool line 25', msg)
-                    _window.do_something(msg);
-                    // Respond to background
-                    _window.respond = function (msg) {
-                        port.postMessage(msg);
-                    };
+                    console.log('data in devtoool line 31', data)
+                    console.log('mssssg in devtoool line 32', msg)
                 }
             });
 
             port.onMessage.addListener(msg => {
+                console.log('immmm in the port on Messaage line 40', msg);
+
                 // Write information to the panel, if exists.
                 // If panel does not exist (yet), the data will be queued.
                 console.log('message from the devtools', msg);
-                console.log(_window, 'im the _window');
-                // if (_window && msg.data) {
-                //     console.log('went inside line 40')
-                //     _window.do_something(msg);
-                // }
+                console.log('windoewwww from the devtools', window);
+
+                if (_window && msg.data) {
+                    console.log('went inside line 51')
+                    port.postMessage({
+                        name: 'sendData',
+                        data: msg,
+                    })
+                }
                 if (_window && msg.stateHasChanged) {
                     console.log('we made it inside if statement in DEVTOOLSSS!!!!!!!!!!!!!!!!!!')
- 
-                    _window.do_somethingElse(msg)
                 }
                 else {
+                    console.log('we made it inside ELSEEE statement in DEVTOOLSSS!!!!!!!!!!!!!!!!!!', msg)
                     data.push(msg);
+                    port.postMessage({
+                        name: 'sendData',
+                        data: msg,
+                    })
                 }
             });
 
@@ -54,6 +65,8 @@ function createPanel() {
                 name: 'connect',
                 tabId: chrome.devtools.inspectedWindow.tabId,
             })
+
+
         });
 };
 
