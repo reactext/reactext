0d248984cfc6035709ce625663c48f1799b5746b
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-05-03 11:32:46 -0700

    passed down stateChanges as props to LogContainer

--- public/App.jsx
+++ public/App.jsx
@@ -6,46 +6,47 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialState : '',
-      stateChangesList: [],
+      initialState: '',
+      // stateChangesList: [],
     };
   }
 
   saveInitialState(prop) {
-    console.log('innnnnnn saveIS function', prop)
+    console.log('initial state CHECKKKKK', this.state.initialState)
+
     if (typeof this.state.initialState === 'string') {
       let initialState = prop;
-      console.log('initialState', initialState)
       this.setState({ initialState })
     }
-    console.log('updatedState', this.state.initialState)
-    // let stringPage = JSON.stringify(this.props.stateChanges);
-    // console.log(stringPage, '<-----------STRINGPAGE')
-  }
+    console.log('initial state CHECKKKKK AFTERRR', this.state.initialState)
 
-  componentDidMount(){
-    this.saveInitialState(this.props.initState.data);
   }
 
   // saveStateChanges(prop) {
   //   console.log('innnnnnn SaveStateChanges function', prop)
-  //   if (typeof prop !== 'string') {
-  //     let oldSC = prop;
-  //     console.log(oldSC, '<-----------oldSC')
-  //     let stateChangesList = oldSC.push(page);
-  //     this.setState({ stateChangesList })
-  //   }
-  //   // let stringPage = JSON.stringify(this.props.stateChanges);
-  //   // console.log(stringPage, '<-----------STRINGPAGE')
+  //   console.log('innnnnnn SaveStateChanges JSON', JSON.stringify(prop))
+
+  //   let oldSC = this.state.stateChangesList;
+  //   console.log(oldSC, '<-----------oldSC')
+  //   // let stateChangesList = oldSC.push();
+  //   // this.setState({ stateChangesList })
   // }
 
+  componentDidMount() {
+    console.log(this.props.initState, '<------ initState inside component didMount')
+
+    this.saveInitialState(this.props.initState.data);
+    // console.log(this.props.stateChanges, '<------ inside component didMount')
+    // this.saveStateChanges(this.props.stateChanges)
+  }
+
   render() {
-    console.log('immmmmmmmm PROPSSSS innnnnnn appp.jsx', this.props)
+    console.log('immmmmmmmm PROPSSSS innnnnnn APPPPPP.jsx', this.props)
     return (
       <div>
         <h4>Reactext</h4>
         <VisContainer />
-        <LogContainer initState ={this.state.initialState} stateChangesList ={this.state.stateChangesList} />
+        <LogContainer initState={this.state.initialState} stateChangesList={this.props.stateChanges} />
       </div>
     );
   }
