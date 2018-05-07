7af0ad0a50c0f30d3acb7e394cb1cc62a7847c61
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-05-07 16:20:55 -0700

    cleaned out console.logs

--- public/index.js
+++ public/index.js
@@ -3,9 +3,7 @@ import { render } from 'react-dom';
 import App from './App.jsx';
 
 let renderApp = function renderApp(prop1, prop2, prop3) {
-    console.log('PROP 1 in render app PROP!!!!', prop1)
-    console.log('PROP 2 in render app', prop2)
-    console.log('PROP 3 in render app', prop3)
+    console.log('PROPs render app PROP!!!!', prop1, prop2, prop3)
     return render(
         <App initState={prop1} stateChanges={prop2} modState={prop3}/>
         , document.getElementById('container')
