1a41b3ff28e8e8b697518b4e7ec5a90a47594675
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-05-07 20:20:27 -0700

    add buttons with onClick function as callback

--- public/NavBar.jsx
+++ public/NavBar.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
-    constructor(props) {
-      super(props);
-    }
-  
-    render() {
-      console.log('immmmmmmmm PROPSSSS innnnnnn appp.jsx', this.props)
-      return (
-        <div className='navBar' style={{backgroundColor:'lightBlue'}}>
-          <h3>NavBar</h3>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
   }
-  
-  export default NavBar;
+
+  render() {
+    console.log('immmmmmmmm PROPSSSS innnnnnn appp.jsx', this.props)
+    return (
+      <div className='navBar' style={{ backgroundColor: 'lightBlue' }}>
+        <h3>NavBar</h3>
+        <button id='initial' onClick={()=>this.props.onClick('initial')}>Initial State</button>
+        <button id='log' onClick={()=>this.props.onClick('log')}>Log</button>
+        <button id='provider' onClick={()=>this.props.onClick('provider')}>Provider</button>
+        <button id='consumer' onClick={()=>this.props.onClick('consumer')}>Consumer</button>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
